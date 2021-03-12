603122cbbac42a150140cc4f543a5d77bdbe7487
Leftyxiv <manny@aledoux.net>
2021-03-12 18:17:47 -0500

    added all views to the rest framework

--- tweets/views.py
+++ tweets/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse,Http404,JsonResponse
 from django.shortcuts import render,redirect
 from django.utils.http import is_safe_url
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from .forms import TweetForm
 from .models import Tweet
 from .serializers import TweetSerializer
@@ -13,11 +16,29 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 def home_view(request,*args,**kwargs):
   return render(request, "pages/home.html", context={}, status=200)
 
+@api_view(['POST']) #hhtp method the client == ppst
 def tweet_create_view(request,*args,**kwargs):
   serializer = TweetSerializer(data=request.POST or None)
-  if serializer.is_valid():
+  if serializer.is_valid(raise_exception=True):
     serializer.save(user=request.user)
-  return JsonResponse({}, status=400)
+    return Response(serializer.data,status=201)
+  return Response({}, status=400)
+
+@api_view(['GET'])
+def tweet_list_view(reques,*args,**kwargs):
+  qs = Tweet.objects.all()
+  serializer = TweetSerializer(qs,many=True)
+  return Response(serializer.data, status=200)
+
+@api_view(['GET'])
+def tweet_detail_view(reques,tweet_id,*args,**kwargs):
+  qs = Tweet.objects.all()
+  if not qs.exists():
+    return Response({}, status=404)
+  obj = qs.first()
+  serializer = TweetSerializer(obj)
+  return Response(serializer.data, status=200)
+
 
 def tweet_create_view_pure_django(request,*args,**kwargs):
   """ 
@@ -46,7 +67,10 @@ def tweet_create_view_pure_django(request,*args,**kwargs):
       return JsonResponse(form.errors, status=400)
   return render(request, 'components/form.html', context={"form": form})
 
-def tweet_list_view(request,*args,**kwargs):
+
+
+
+def tweet_list_view_pure_django(request,*args,**kwargs):
   """ 
   REST API VIEW
   consume by js
@@ -59,7 +83,7 @@ def tweet_list_view(request,*args,**kwargs):
   }
   return JsonResponse(data)
 
-def tweet_detail_view(request,tweet_id,*args,**kwargs):
+def tweet_detail_view_pure_django(request,tweet_id,*args,**kwargs):
   """ 
   REST API VIEW
   consume by js
