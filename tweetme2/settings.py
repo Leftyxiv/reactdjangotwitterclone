f6e4f9a70453d8c997ac06a4683cf14511998e4f
Leftyxiv <manny@aledoux.net>
2021-03-12 13:20:44 -0500

    got rest api view in

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = '!=v-wt0x0pq2pbg)@nnhi+hs1$)@f-trr%f!90ic1c8=5!4^@$'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.aledoux.net']
+LOGIN_URL = "/login"
+MAX_TWEET_LENGTH = 240
 
 
 # Application definition
@@ -36,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # third-party
+    'rest_framework',
+    # internal
     'tweets',
 ]
 
--- tweets/forms.py
+++ tweets/forms.py
@@ -11,6 +11,5 @@ class TweetForm(forms.ModelForm):
   def clean_content(self):
     content = self.cleaned_data.get("content")
     if len(content) > MAX_LENGTH:
-      print('error')
       raise forms.ValidationError("This tweet is too long!")
     return content
