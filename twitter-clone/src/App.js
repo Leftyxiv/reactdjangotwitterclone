29c372aca36133dbb73c684f8e7457649f0ceac3
Leftyxiv <manny@aledoux.net>
2021-03-14 19:28:36 -0400

    added react app and not gets the tweet listy from the bakend

--- twitter-clone/src/App.js
+++ twitter-clone/src/App.js
@@ -1,7 +1,50 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const Tweet = (props) => {
+  const { tweet } = props;
+  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+  return <div className={className}>
+    <p>
+      { tweet.content }
+    </p>
+  </div>
+}
 function App() {
+  const [tweets, setTweets] = useState([]);
+  
+  const loadTweets = (callback) => {
+    
+    const xhr = new XMLHttpRequest()
+    const method = 'GET'
+    const url = 'http://localhost:8000/api/tweets/'
+    const responseType = 'json'
+    
+    xhr.responseType = responseType;
+    xhr.open(method, url)
+    xhr.onload = function() {
+      callback(xhr.response, xhr.status)
+    }
+    xhr.onerror = (e) => {
+      callback({"message": "There was an error"}, 400)
+    }
+    xhr.send()
+    
+  }
+  //loadTweets(document.getElementById('tweets'))
+  
+  useEffect(()=> {
+    const myCallback = (res, status) => {
+      if(status === 200){
+        setTweets(res)
+      } else {
+        alert('ERROR WILL ROBINSON')
+      }
+    }
+  loadTweets(myCallback)
+  },[])
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +52,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {tweets.map((tweet, index) => {
+            return <Tweet tweet={tweet} key={tweet.id}/>
+          })}
       </header>
     </div>
   );
